89f737d8f9d3851b3d273cc46be8f012350b326b
Chris Matala <ch.matala@gmail.com>
2024-11-17 19:03:07 +0100

    Vision base

--- Computer Vision/aruco.py
+++ Computer Vision/aruco.py
@@ -1,14 +1,19 @@
 import cv2 as cv
 import numpy as np
 
-aruco_dictionary = cv.aruco.DICT_7X7_50
+# Calibration parameters yaml file
+camera_calibration_parameters_filename = 'calibration_chessboard.yaml'
   
-def get_arucos(frame):
-    """
-    Main method of the program.
-    """
+def get_arucos(frame, marker_size):
+    # Load the camera parameters from the saved file
+    cv_file = cv.FileStorage(
+        camera_calibration_parameters_filename, cv.FILE_STORAGE_READ) 
+    camera_matrix = cv_file.getNode('K').mat()
+    dist_coeffs = cv_file.getNode('D').mat()
+    cv_file.release()
+    
+    aruco_dictionary = cv.aruco.DICT_7X7_50
     # Load the ArUco dictionary
-    print("ARUCO: '{}'".format(aruco_dictionary))
     aruco_dictionary = cv.aruco.getPredefinedDictionary(aruco_dictionary)
     aruco_parameters =  cv.aruco.DetectorParameters()
    
@@ -18,11 +23,24 @@ def get_arucos(frame):
         
     # Check that at least one ArUco marker was detected
     if len(corners) > 0:
+        # Draw a square around detected markers in the video frame
+        cv.aruco.drawDetectedMarkers(frame, corners, ids)
+        # Get the rotation and translation vectors
+        rvecs, tvecs, obj_points = cv.aruco.estimatePoseSingleMarkers(
+            corners,
+            marker_size,
+            camera_matrix,
+            dist_coeffs)
         # Flatten the ArUco IDs list
         ids = ids.flatten()
         
         # Loop over the detected ArUco corners
+        i = 0
+        aruco_markers = {}
         for (marker_corner, marker_id) in zip(corners, ids):
+            # Draw marker axes
+            cv.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.05)
+            
             # Extract the marker corners
             corners = marker_corner.reshape((4, 2))
             (top_left, top_right, bottom_right, bottom_left) = corners
@@ -36,6 +54,14 @@ def get_arucos(frame):
             # Calculate the center of the ArUco marker
             center_x = int((top_left[0] + bottom_right[0]) / 2.0)
             center_y = int((top_left[1] + bottom_right[1]) / 2.0)
-    return [corners,ids]
+            aruco_markers[marker_id] = [center_x,
+                                        center_y,
+                                        top_left,
+                                        top_right,
+                                        bottom_right,
+                                        bottom_left]
+            i+=1
+        return aruco_markers
+    return {}
 
     
--- Computer Vision/vision.py
+++ Computer Vision/vision.py
@@ -1,6 +1,35 @@
 import os
 import cv2 as cv
 import numpy as np
+import aruco
+from scipy.spatial.transform import Rotation as R
+import keyboard
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# === CONSTANTS ===
+MARKER_SIZE_ROBOT = 0.1     # Side length of the ArUco marker in meters 
+FIRST_FRAME = 50            # First frame to analyse
+
+def vision_start():
+    # Start the video stream
+    capture = cv.VideoCapture(0)
+    
+    # Drop the first 50 frame
+    for _ in range(FIRST_FRAME):
+        capture.read()
+    
+    # Scan Robot Aruco
+    ret, frame = capture.read()
+    markers = aruco.get_arucos(frame, MARKER_SIZE_ROBOT)
+    cv.imshow('frame',frame)
+    
+    # Scan Map Aruco
+    
+    cv.waitKey(1)
+    capture.release()
+    cv.destroyAllWindows()
+   
+if __name__ == '__main__':
+    os.system('cls')
+    vision_start()
