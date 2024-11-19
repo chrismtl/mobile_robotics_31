582dbc45660c1f983a7165db78a6b0126c48cb98
zacbrld <zacharie.bourlard@epfl.ch>
2024-11-19 17:20:50 +0100

    Mod_nav

--- Navigation/Navigation_utils.py
+++ Navigation/Navigation_utils.py
@@ -1,8 +1,23 @@
 import tdmclient.notebook
 
-def global_navigation():
-    
+
+def global_navigation(speed0):
+    leds_top = [0,0,0]
+    motor_left_target = speed0
+    motor_right_target = speed0
 
 
 def collision_avoidance():
+    leds_top = [50,50,50]
 
+    w_l = [40,  20, -20, -20, -40,  30, -10]
+    w_r = [-40, -20, -20,  20,  40, -10,  30]
+    
+    actuation = [0,0]
+
+    sensor_translation = 200
+
+    for i in range (0, len(prox))
+        
+        motor_left_target = speed0
+        motor_right_target = speed0
