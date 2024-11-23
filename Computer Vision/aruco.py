e027e168e6e7b2c3a227b3540e1821de5278ae18
Chris Matala <ch.matala@gmail.com>
2024-11-23 19:19:01 +0100

    Testing

--- Computer Vision/aruco.py
+++ Computer Vision/aruco.py
@@ -1,5 +1,10 @@
 import cv2 as cv
 import numpy as np
+import geometry as geom
+
+# CONSTANTS
+CV_DRAW = True
+ARUCO_DICTIONARY = cv.aruco.DICT_4X4_50
 
 # Calibration parameters yaml file
 camera_calibration_parameters_filename = 'calibration_chessboard.yaml'
@@ -12,9 +17,8 @@ def get_arucos(frame, marker_size):
     dist_coeffs = cv_file.getNode('D').mat()
     cv_file.release()
     
-    aruco_dictionary = cv.aruco.DICT_7X7_50
     # Load the ArUco dictionary
-    aruco_dictionary = cv.aruco.getPredefinedDictionary(aruco_dictionary)
+    aruco_dictionary = cv.aruco.getPredefinedDictionary(ARUCO_DICTIONARY)
     aruco_parameters =  cv.aruco.DetectorParameters()
    
     # Detect ArUco markers in the video frame
@@ -24,7 +28,7 @@ def get_arucos(frame, marker_size):
     # Check that at least one ArUco marker was detected
     if len(corners) > 0:
         # Draw a square around detected markers in the video frame
-        cv.aruco.drawDetectedMarkers(frame, corners, ids)
+        if CV_DRAW: cv.aruco.drawDetectedMarkers(frame, corners, ids)
         # Get the rotation and translation vectors
         rvecs, tvecs, obj_points = cv.aruco.estimatePoseSingleMarkers(
             corners,
@@ -39,11 +43,11 @@ def get_arucos(frame, marker_size):
         aruco_markers = {}
         for (marker_corner, marker_id) in zip(corners, ids):
             # Draw marker axes
-            cv.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.05)
+            if CV_DRAW: cv.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.05)
             
             # Extract the marker corners
-            corners = marker_corner.reshape((4, 2))
-            (top_left, top_right, bottom_right, bottom_left) = corners
+            treated_corners = marker_corner.reshape((4, 2))
+            (top_left, top_right, bottom_right, bottom_left) = treated_corners
                 
             # Convert the (x,y) coordinate pairs to integers
             top_right = (int(top_right[0]), int(top_right[1]))
@@ -54,13 +58,17 @@ def get_arucos(frame, marker_size):
             # Calculate the center of the ArUco marker
             center_x = int((top_left[0] + bottom_right[0]) / 2.0)
             center_y = int((top_left[1] + bottom_right[1]) / 2.0)
-            aruco_markers[marker_id] = [center_x,
-                                        center_y,
-                                        top_left,
-                                        top_right,
-                                        bottom_right,
-                                        bottom_left]
+                        
+            # Store the rotation information
+            rotation_matrix = np.eye(4)
+            rotation_matrix = cv.Rodrigues(np.array(rvecs[i][0]))[0]
+            (rx,ry,rz) = geom.get_rotations_chat(rotation_matrix)
+            aruco_markers[int(marker_id)] = [[center_x,center_y],
+                                             [rx,ry,rz],
+                                             np.round(marker_corner).astype(np.int32)]
+            
             i+=1
+            
         return aruco_markers
     return {}
 
--- Computer Vision/vision.py
+++ Computer Vision/vision.py
@@ -3,33 +3,140 @@ import cv2 as cv
 import numpy as np
 import aruco
 from scipy.spatial.transform import Rotation as R
-import keyboard
+import geometry as geom
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.system('cls')
+
+# TESTING
+TESTING = True
+TEST_FRAME = cv.imread('test_map.png')
 
 # === CONSTANTS ===
 MARKER_SIZE_ROBOT = 0.1     # Side length of the ArUco marker in meters 
 FIRST_FRAME = 50            # First frame to analyse
+ROBOT_RADIUS = 50
 
-def vision_start():
-    # Start the video stream
-    capture = cv.VideoCapture(0)
+def rescaleFrame(frame, scale=0.75):
+    height = int(frame.shape[0] * scale)
+    width = int(frame.shape[1] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+class Map:
+    def __init__(self, frame):
+        self.frame = frame
+        self.robot = np.zeros((1,3))
+        self.destination = np.zeros(3)
+        self.obstacles = []
+
+    def info(self):
+        print("===== MAP INFO =====")
+        print("ROBOT:")
+        print(f"Position: {self.robot[0]}")
+        print(f"Orientation: {self.robot[1]}\n")
+        print("DESTINATION:")
+        print(f"Position: {self.destination[0]}")
+
+    def set_frame(self,new_frame):
+        self.frame = new_frame
     
-    # Drop the first 50 frame
-    for _ in range(FIRST_FRAME):
-        capture.read()
+    def get_frame(self):
+        return self.frame
     
-    # Scan Robot Aruco
-    ret, frame = capture.read()
-    markers = aruco.get_arucos(frame, MARKER_SIZE_ROBOT)
-    cv.imshow('frame',frame)
+    def scan_arucos(self):
+        aruco_markers = aruco.get_arucos(self.frame, MARKER_SIZE_ROBOT)
+        
+        if len(aruco_markers):
+            # Debug
+            if 0 in aruco_markers.keys():
+                print("Robot found")
+                self.robot = aruco_markers[0]
+            else:
+                print("Could not find Robot")
+            if 1 in aruco_markers.keys():
+                print("Destination found")
+                self.destination = aruco_markers[1]
+            else:
+                print("Could not find Destination")
+        else:
+            print("No aruco tags detected")
     
-    # Scan Map Aruco
-    
-    cv.waitKey(1)
-    capture.release()
-    cv.destroyAllWindows()
+    def detect_obstacles(self):
+        # Create local copy of the frame that we will use for treatment
+        frame = self.frame.copy()
+        
+        # Mask out the robot and the destination
+        mask_robot = [geom.augmented_polygon(self.robot[2][0], 6)]
+        mask_destination = [geom.augmented_polygon(self.destination[2][0], 6)]
+        cv.fillPoly(frame, mask_robot, (255, 255, 255))
+        cv.fillPoly(frame, mask_destination, (255, 255, 255))
+        
+        # Detect edges
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frame = cv.Canny(frame, 50, 150)
+        
+        # Detect contours
+        contours, _ = cv.findContours(frame, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        
+        # Show edge detection
+        cv.imshow("Edges", frame)
+
+        # Approximate the contour to a polygon and extract corners
+        for contour in contours:
+            epsilon = 0.025 * cv.arcLength(contour, True)  # 2% of the perimeter
+            approx_corners = cv.approxPolyDP(contour, epsilon, True)
+        
+            if len(approx_corners) >= 3:  # Ensure it's a valid polygon
+                corners = approx_corners.reshape(-1, 2) # Extract the corners
+                corners = geom.remove_close_points(corners) # Remove duplicates
+                corners = geom.augmented_polygon(corners,ROBOT_RADIUS) # Compute augmented obstacle
+                self.obstacles.append(corners)  # Add the obstacles to our obstacle list
+
+
+    def draw(self):
+        # Draw robot 
+        cv.drawContours(self.frame, self.robot[2], -1, (255,0,0), 5)
+        # Draw the robot orientation
+        
+        # Draw destination  
+        cv.drawContours(self.frame, self.destination[2], -1, (0,255,0), 5)
+        
+        # Draw obstacle
+        for obstacle in self.obstacles:
+            cv.polylines(frame, [obstacle], isClosed=True, color=(0, 0, 255), thickness=5)
+
+        
+    def visibility_graph():
+        return []
+        
+    def output_navigation(self):
+        # Create visibility graph
+        
+        return []
    
 if __name__ == '__main__':
     os.system('cls')
-    vision_start()
+    if not(TESTING):
+        capture = cv.VideoCapture(0)
+        for _ in range(FIRST_FRAME):
+            capture.read()
+        frame = capture.read()
+    else: 
+        frame = TEST_FRAME
+        frame = rescaleFrame(frame,0.3)
+    
+    map = Map(frame)    
+    map.scan_arucos()
+    map.detect_obstacles()   
+    map.draw()    
+    map.info()
+    
+    cv.imshow('frame',map.get_frame())
+        
+    cv.waitKey(0)
+    if not(TESTING): capture.release()
+    cv.destroyAllWindows()
+    
+    
