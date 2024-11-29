21fecf881c6a4ed6622704732d88209570861374
Meake08 <109254909+Meake08@users.noreply.github.com>
2024-11-29 20:12:02 +0100

    implementation of the kalman filter

--- computer_vision/constants.py
+++ computer_vision/constants.py
@@ -6,7 +6,7 @@ MIN_OBSTACLE_SEGMENT_LENGTH = 10
 # Vision
 LINE_THICKNESS = 1
 MARKER_SIZE_ROBOT = 0.037    # Side length of the ArUco marker in meters 
-FIRST_FRAME = 30            # First frame to analyse
+FIRST_FRAME = 15            # First frame to analyse
 ROBOT_RADIUS = 200
 MIN_AREA = 10
 MAX_AREA = 180000
