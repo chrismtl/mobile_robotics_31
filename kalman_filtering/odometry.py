c77930868f1955447d8a0d637f003b66c058b59a
zacbrld <zacharie.bourlard@epfl.ch>
2024-12-05 21:31:25 +0100

    DERNIER KALMAN PLUS CHANGER

--- kalman_filtering/odometry.py
+++ kalman_filtering/odometry.py
@@ -13,15 +13,15 @@ def odometry(v_left, v_right, x_prev, y_prev, theta_prev, dt):
     """
     # Compute the linear velocity (in mm/s) and angular velocity (in rad/s) of the Thymio
     v = ((0.5*(v_left+v_right))*SPEED_COEFF)*PIXEL_PER_CM # [px/s]
-    omega = (((0.5/WHEEL_AXLE_LENGTH)*(v_left-v_right))*SPEED_COEFF)*PIXEL_PER_CM # [rad/s]
-    #print("Motors speed", [v_left, v_right])
-    #print("Robot angular velocity", omega)
+    omega = (((0.5/WHEEL_AXLE_LENGTH)*(v_right-v_left))*SPEED_COEFF)*PIXEL_PER_CM # [rad/s]
+    print("Motors speed", [v_left, v_right])
+    print("Robot angular velocity", omega)
 
     # Update position x y (in mm)
     x = int(x_prev + v * dt * np.cos(theta_prev)) 
     y = int(y_prev + v * dt * np.sin(theta_prev))
 
     # Update orientation theta (in rad)
-    theta = theta_prev + omega * dt # Minus sign due to the y-axis orientation in the camera frame, pointing down instead of up
+    theta = theta_prev - omega * dt # Minus sign due to the y-axis orientation in the camera frame, pointing down instead of up
     
-    return x, y, theta, v, omega
+    return x, y, theta
